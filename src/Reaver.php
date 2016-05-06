26eeb1badde9e35817ca1bc62d465f3068f9d9c5
Jeremy Menicucci <menicucci_207@yahoo.com>
2016-05-06 11:39:53 -0600

    Setting up multple methods

--- src/Reaver.php
+++ src/Reaver.php
@@ -2,6 +2,7 @@
 
 namespace Reaver;
 
+use Carbon\Carbon;
 use GuzzleHttp\Client;
 use GuzzleHttp\Promise;
 use GuzzleHttp\Psr7\Request;
@@ -13,42 +14,56 @@ class Spider {
 
 	public $url;
 	public $links;
+	public $base;
 
 	public function __construct()
 	{
-		$this->url = 'http://www.twitter.com';
+		echo '['.Carbon::now().'] Initializing Reaver...'.PHP_EOL;
 	}
 
 	public function __destruct()
 	{
-		var_dump($this->links);
+	
 	}
 
-	public function crawl()
+	public function setUrl($url)
 	{
-		$client = new Client(['base_uri' => $this->url]);
+		$this->url = $url;
+		$this->base = ['base_uri' => $this->url];
+	}
+
+	public function fetch()
+	{
+		$client = new Client($this->base);
 
 		$promise = $client->getAsync($this->url)->then(function($response) {
+
+			echo '['.Carbon::now().'] ('.$response->getStatusCode().') >> '.$this->url.PHP_EOL;
+
 			$content = $response->getBody()->getContents();	
-			$crawler = new Crawler($content, $this->url);
 
-			$title = $crawler->filterXpath('//title')->text();
-			$links = $crawler->filterXpath('//a')->each(function(Crawler $node, $i) {
-				echo $node->attr('href') . PHP_EOL;
-
-				$text = trim($node->text());
-				$href = url_to_absolute($this->url, $node->attr('href'));
-				$href = rtrim($href, '#');
-				$href = rtrim($href, '/');
-
-				$this->links[] = [
-					$text => $href
-				];
-			});
-
+			$this->crawl($content);
 		});
 
 		$promise->wait();
+	}	
+
+	public function crawl($html)
+	{
+		$crawler = new Crawler($html, $this->url);
+		$title = $crawler->filterXpath('//title')->text();
+		$links = $crawler->filterXpath('//a')->each(function(Crawler $node, $i) {
+			$href = url_to_absolute($this->url, $node->attr('href'));
+			$href = rtrim($href, '#');
+			$href = rtrim($href, '/');
+			$this->links[] = $href;
+		});	
 	}
+
+	public function run()
+	{
+		$this->fetch();
+	}
+
 }
 
