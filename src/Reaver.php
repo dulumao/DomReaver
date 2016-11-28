b9661945b8e299ddd2105559cfd30f5baffb0731
Jeremy Menicucci <jeremy@jeremyam.com>
2016-11-28 16:16:33 -0700

    Updated methods, can call methods outside of loop

--- src/Reaver.php
+++ src/Reaver.php
@@ -56,15 +56,11 @@ class Spider {
 	 * @param  string
 	 * @return array
 	 */
-	public function crawl($html, $url)
+	public function crawl($html, $url, $base)
 	{
 		return $this->crawler = new Crawler($html, $this->url);
 
-		/*$title = count($crawler->filterXPath('//title')) != 0 ? $crawler->filterXPath('//title')->text() : $this->url;
-        $metas = $crawler->filterXPath('//meta[@name="description"]');
-        $meta = count($metas) !== 0 ? $crawler->filterXPath('//meta[@name="description"]')->attr('content') : '';
-        $meta = !empty($meta) ? $meta : substr($crawler->filterXPath('//body')->text(), 0, 200);
-
+		/*
         $this->site[] = [
         	'url' => $url,
         	'base' => $base, 
@@ -87,7 +83,6 @@ class Spider {
         $this->links = array_values($this->links);*/
 	}
 
-
 	public function title()
 	{
 		$title = count($this->crawler->filterXPath('//title')) != 0 ? 
@@ -96,6 +91,44 @@ class Spider {
 		return $title;
 	}	
 
+	public function description()
+	{
+		$metas = $this->crawler->filterXPath('//meta[@name="description"]');
+        $description = count($metas) !== 0 ? $this->crawler->filterXPath('//meta[@name="description"]')->attr('content') : '';
+        $description = !empty($description) ? $description : substr($this->crawler->filterXPath('//body')->text(), 0, 200);
+        return $description;
+	}
+
+	public function links()
+	{
+		$links = $this->crawler->filterXpath('//a')->each(function(Crawler $node, $i) {
+			$href = url_to_absolute($this->url, $node->attr('href'));
+			if(checkUrl($href) && !checkImage($href) && !is_null($href)) {
+				$href = rtrim($href, '#');
+				$href = rtrim($href, '/');
+				$this->links[] = $href;
+			}
+		});	
+
+		$this->links = is_array($this->links) ? array_unique($this->links) : [$this->links];
+        $this->links = array_filter($this->links);
+        $this->links = array_values($this->links);
+
+		return $this->links;
+	}
+
+	public function store()
+	{
+		$this->site[] = [
+        	'url' => $this->url,
+        	'base' => $this->base, 
+        	'title' => $this->title(), 
+        	'description' => $this->description(), 
+        	'html' => preg_replace('/(\s)+/', ' ', strip_tags($this->crawler->html()))
+        ];
+        return $this->site;
+	}
+
 	/**
 	 * Follows links
 	 * TODO: will set up a method for abiding by robots directives
@@ -103,6 +136,8 @@ class Spider {
 	 */
 	public function follow()
 	{
+		if(empty($this->links)) $this->links();
+		
 		foreach($this->links as $link) {
 			if(in_array($link, $this->followed)) continue;
 			$this->url = $link;
