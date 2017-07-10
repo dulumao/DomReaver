9976199528926bbe0c8817b382817092c703b3be
Jeremy Menicucci <jeremy@jeremyam.com>
2017-07-10 12:12:06 -0600

    Added follow ability back in, using arg as well

--- src/Reaver.php
+++ src/Reaver.php
@@ -6,24 +6,26 @@ use Carbon\Carbon;
 use GuzzleHttp\Client;
 use GuzzleHttp\Promise;
 use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
 use Symfony\Component\DomCrawler\Crawler;
 
 class Spider {
 
 	protected $client;
-	protected $promise;
-	protected $promises;
 	protected $url;
-	protected $links;
+	protected $links = [];
 	protected $base;
-	protected $site;
-	protected $followed;
-	protected $robots;
-	protected $html;
+	protected $args;
+	protected $i;
 
 	public function __construct()
 	{
-		$this->client = new Client();
+		$this->args = $_GET;
+		array_push($this->links, $this->args['--url']);
+		$this->base = ['base_uri' => $this->args['--url']];
+		$this->client = new Client($this->base);
+		$this->url = $this->args['--url'];
+		$this->i = 0;
 	}
 
 	public function __destruct()
@@ -32,45 +34,43 @@ class Spider {
 	}
 
 	public function init() {
-		//$this->links[] = $url;
-		//$this->base = ['base_uri' => $url];
-		var_dump($_GET);
+		
+		$this->fetch($this->url);
+	}
+
+	private function hasArg($arg) 
+	{
+		if(array_key_exists($arg, $this->args)) {
+			return true;
+		} 
+		return false;
+	}
+
+	private function fetch($url)
+	{
+		try {
+			$request = new Request('GET', $url);
+			$promise = $this->client->sendAsync($request)->then(function($response) use ($request) {
+				echo '['.Carbon::now().'] ('.$response->getStatusCode().') >> '.$request->getUri().PHP_EOL;
+				$content = $response->getBody()->getContents();	
+				$this->crawl($content, $request->getUri(), $this->base);
+				$this->followed[] = $request->getUri();
+			});
+			
+			$promise->wait();
+		} catch(\GuzzleHttp\Exception\ClientException $e) {
+			var_dump($e->getMessage());
+		}
 	}
 
 	private function follow()
 	{
-		$this->fetch();
-	}
-
-	private function fetch()
-	{
-		foreach($this->links as $link) {
-			$request = new Request('GET', $link);
-			$promise = $this->client->sendAsync($request)->then(function($response) use ($link) {
-				echo '['.Carbon::now().'] ('.$response->getStatusCode().') >> '.$link.PHP_EOL;
-				//$content = $response->getBody()->getContents();	
-				//var_dump($content);
-			});
-		}
-		$promise->wait();
+		$this->fetch($this->links[$this->i++]);
 	}
 
 	private function crawl($html, $url, $base)
 	{
 		$crawler = new Crawler($html, $this->url);
-		$title = count($crawler->filterXPath('//title')) != 0 ? $crawler->filterXPath('//title')->text() : $this->url;
-        $metas = $crawler->filterXPath('//meta[@name="description"]');
-        $meta = count($metas) !== 0 ? $crawler->filterXPath('//meta[@name="description"]')->attr('content') : '';
-        $meta = !empty($meta) ? $meta : substr($crawler->filterXPath('//body')->text(), 0, 200);
-
-        $this->site[] = [
-        	'url' => $url,
-        	'base' => $base, 
-        	'title' => $title, 
-        	'description' => $meta, 
-        	'html' => preg_replace('/(\s)+/', ' ', strip_tags($crawler->html()))
-        ];
-
 		$links = $crawler->filterXpath('//a')->each(function(Crawler $node, $i) {
 			$href = url_to_absolute($this->url, $node->attr('href'));
 			if(checkUrl($href) && !checkImage($href) && !is_null($href)) {
@@ -79,9 +79,11 @@ class Spider {
 				$this->links[] = $href;
 			}
 		});	
-
 		$this->links = is_array($this->links) ? array_unique($this->links) : [$this->links];
         $this->links = array_filter($this->links);
         $this->links = array_values($this->links);
+        if($this->hasArg('-f')) {
+        	$this->follow();
+        }
 	}
 }
